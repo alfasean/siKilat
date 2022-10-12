4fecb50248c54c792e365d0269ed03b6c7f91d63
Your Name <you@example.com>
2022-10-13 01:24:44 +0800

    add back to top button

---
+++ script.js
@@ -0,0 +1,13 @@
+var backToTop = document.querySelector('.back-to-top');
+
+        window.addEventListener('scroll', () => {
+        if ( this.scrollY >= 40 ) {
+            backToTop.classList.add('show');
+
+            backToTop.addEventListener('click', () => {
+            window.scrollTo({top: 0});
+            })
+        } else {
+            backToTop.classList.remove('show');
+        }
+        });
